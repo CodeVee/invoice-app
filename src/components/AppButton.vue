a80ce06ee095979d7f7ae6c087c1b1fb2ec3dc5b
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-11-14 13:21:07 +0100

    added app button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :class="btnClass" class="py-1.6 px-2.4 text-fl font-bold rounded-4ls">{{text}}</button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+
+interface Props {
+    text: string;
+    type: 'primary' | 'secondary' | 'tetiary'
+}
+const props = defineProps<Props>(),
+btnClass = computed(() => {
+    switch (props.type) {
+        case 'primary':
+            return 'text-white bg-purple';
+        case 'secondary':
+            return 'text-white bg-red';
+        default:
+            return 'text-blue-gray bg-blue-vlight';
+    }
+})
+</script>
