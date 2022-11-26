9816820c0e9ae75596beaa16b0d4b07cfd560ca0
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-11-26 23:28:09 +0100

    added textbox

---
+++ src/components/TextBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-col gap-4">
+        <label v-if="labelText" :for="id || labelText" class="text-fl font-medium text-blue-gray">{{labelText}}</label>
+        <input 
+            :id="id || labelText"
+            class="border border-blue-light rounded-lg text-black font-bold text-fl px-8 h-4.8 focus-visible:outline-none"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target ? ($event.target as HTMLInputElement).value : '')" 
+            type="text"
+        >
+    </div>
+    
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+    modelValue?: any;
+    labelText?: string;
+    id?: string;
+    type?: 'text' | 'date';
+}
+interface Emits {
+    (e: 'update:modelValue'): any
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
