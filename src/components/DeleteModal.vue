bde0482285e15f0504388625066b2f118442a9e2
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-11-27 17:13:29 +0100

    modal darkmode

--- src/components/DeleteModal.vue
+++ src/components/DeleteModal.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex justify-center items-center w-full h-full absolute top-0 z-20" @click="$emit('close')">
-        <div class="w-[48rem] rounded-xls shadow-lgs p-4.8 bg-white relative" @click.stop="">
-            <h4 class="text-black font-bold text-ts">Confirm Deletion</h4>
-            <p class=" text-sm leading-md tracking-lg font-medium text-blue-vgray mt-1.2 mb-1.6">Are you sure you want to delete invoice {{invoiceId}}? This action cannot be undone.</p>
+        <div class="w-[48rem] rounded-xls shadow-lgs p-4.8 bg-white dark:bg-blue-vdark relative" @click.stop="">
+            <h4 class="text-black dark:text-white font-bold text-ts">Confirm Deletion</h4>
+            <p class=" text-sm leading-md tracking-lg font-medium text-blue-vgray dark:text-blue-light mt-1.2 mb-1.6">Are you sure you want to delete invoice {{invoiceId}}? This action cannot be undone.</p>
             <div class="flex justify-end gap-0.8">
                 <AppButton text="Cancel" type="tetiary" @btn-click="$emit('close')"/>
                 <AppButton text="Delete" type="secondary"/>
