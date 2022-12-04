8d14000731c5141830b2fbb1cd0232dae0a2a649
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-12-04 11:06:00 +0100

    handled textbox error state

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="flex flex-col gap-4">
-        <label v-if="labelText" :for="id || labelText" class="text-fl font-medium text-blue-gray dark:text-blue-light">{{labelText}}</label>
+        <div  v-if="labelText" class="flex justify-between">
+            <label :for="id || labelText" :class="{'text-red': hasError}" class="text-fl font-medium text-blue-gray dark:text-blue-light">{{labelText}}</label>
+            <span v-if="hasError" class="text-red text-xs leading-xs tracking-ls font-semibold">{{ errorMessage || `can't be empty`}}</span>
+        </div>
+        
         <input 
             :id="id || labelText"
-            class="border border-blue-light caret-purple-light hover:cursor-pointer focus:ring-0 focus:border-purple-light dark:focus:border-purple-light dark:border-blue-dark bg-white dark:bg-blue-vdark rounded-lg text-black dark:text-white font-bold text-fl px-8 h-4.8 focus-visible:outline-none placeholder:text-black/40 dark:placeholder:text-white"
+            :class="{'border-red focus:border-red': hasError , 'border-blue-light focus:border-purple-light dark:focus:border-purple-light dark:border-blue-dark': !hasError}"
+            class="border caret-purple-light hover:cursor-pointer focus:ring-0 bg-white dark:bg-blue-vdark rounded-lg text-black dark:text-white font-bold text-fl px-8 h-4.8 focus-visible:outline-none placeholder:text-black/40 dark:placeholder:text-white"
             :value="modelValue"
             @input="$emit('update:modelValue', $event.target ? ($event.target as HTMLInputElement).value : '')" 
             type="text"
@@ -19,8 +24,9 @@ interface Props {
     modelValue?: any;
     labelText?: string;
     id?: string;
-    type?: 'text' | 'date';
     placeholder?: string
+    hasError?: boolean;
+    errorMessage?: string; 
 }
 interface Emits {
     (e: 'update:modelValue'): any
