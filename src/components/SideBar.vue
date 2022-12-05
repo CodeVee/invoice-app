602e8ac2803a274f370912f1102e5a3b78e9a2c3
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-12-05 12:02:29 +0100

    made sidebar responsive

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -17,14 +17,14 @@
 </script>
 
 <template>
-    <aside class="bg-blue-deep dark:bg-blue-vdark relative z-20 h-32 lg:h-full w-full lg:w-10.3 lg:rounded-r-4xl flex flex-row lg:flex-col justify-between">
-        <div class="relative overflow-y-clip w-32 h-full lg:h-10.3 bg-purple rounded-r-4xl flex items-center justify-center">
+    <aside class="bg-blue-deep dark:bg-blue-vdark relative z-20 h-7.2 md:h-32 lg:h-full w-full lg:w-10.3 lg:rounded-r-4xl flex flex-row lg:flex-col justify-between">
+        <div class="relative overflow-y-clip w-32 lg:w-full h-full lg:h-10.3 bg-purple rounded-r-4xl flex items-center justify-center">
             <img class="relative z-10" src="@/assets/images/logo.svg" alt="logo">
             <div class="absolute left-0 top-[50%] h-full w-full bg-purple-light rounded-l-4xl flex items-center justify-center"></div>
         </div>
         
         <div class="flex flex-row lg:flex-col">
-            <button @click="toggleMode" class="self-center mr-3.2 lg:mb-3.2">
+            <button @click="toggleMode" class="self-center mr-3.2 lg:mr-0 lg:mb-3.2">
                 <img v-show="darkMode" src="@/assets/images/icon-sun.svg" alt="sun">
                 <img v-show="!darkMode" src="@/assets/images/icon-moon.svg" alt="moon">
             </button>
