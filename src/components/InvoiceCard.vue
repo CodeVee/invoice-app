61f6a41d94cc828a3a2269749daf12c4bf6be4e6
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-12-05 12:03:13 +0100

    made invoice cards responsive

--- src/components/InvoiceCard.vue
+++ src/components/InvoiceCard.vue
@@ -17,7 +17,7 @@ viewMore = () => emits('select');
 </script>
 
 <template>
-    <div @click="viewMore" class="bg-white dark:bg-blue-vdark hover:border hover:border-purple flex items-center h-[7.2rem] shadow-lgs pl-[3.2rem] pr-[2.4rem] rounded-xls cursor-pointer active:scale-[.99]">
+    <div @click="viewMore" class="bg-white dark:bg-blue-vdark hover:border hover:border-purple hidden md:flex items-center h-[7.2rem] shadow-lgs pl-3.2 pr-2.4 rounded-xls cursor-pointer active:scale-[.99]">
         <h3 class="text-fl font-bold basis-[10.3rem]">
             <span class="text-blue-gray">#</span>
             <span class=" text-black dark:text-white">{{invoice.id}}</span>
@@ -32,5 +32,26 @@ viewMore = () => emits('select');
         </h3>
         <InvoiceStatus :status="invoice.status"/>
         <img class="ml-8" src="@/assets/images/icon-arrow-right.svg" alt="right-arrow" />
+    </div>
+    <div @click="viewMore" class="bg-white dark:bg-blue-vdark flex flex-col gap-2.4 md:hidden shadow-lgs p-2.4 rounded-xls active:scale-[.99]">
+        <div class="flex justify-between">
+            <h3 class="text-fl font-bold">
+                <span class="text-blue-gray">#</span>
+                <span class=" text-black dark:text-white">{{invoice.id}}</span>
+            </h3>
+            <h3 class="text-blue-mgray dark:text-white text-fl font-medium">{{invoice.clientName}}</h3>
+        </div>
+        <div class="flex justify-between">
+            <div>
+                <h3 class="text-fl font-medium mb-0.8">
+                    <span class="text-blue-gray dark:text-blue-light">Due  </span>
+                    <span class=" text-blue-vgray dark:text-blue-light">{{formatDate(props.invoice.paymentDue)}}</span>
+                </h3>
+                <h3 class="text-black dark:text-white text-sl font-bold">
+                    £ {{formatAmount(invoice.total)}}
+                </h3>
+            </div>
+            <InvoiceStatus :status="invoice.status"/>
+        </div>
     </div>
 </template>
--- src/components/NoInvoice.vue
+++ src/components/NoInvoice.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-col items-center mt-60 lg:mt-0">
+    <div class="flex flex-col items-center mt-32 md:mt-60 lg:mt-0">
         <img src="@/assets/images/illustration-empty.svg" alt="empty">
         <h3 class="text-black font-bold text-tl mt-[6.4rem] mb-2.4">There is nothing here</h3>
-        <h4 class="text-blue-vgray font-medium text-fl text-center"> Create an invoice by clicking the <br><span class="font-bold">New Invoice</span> button and get started</h4>
+        <h4 class="text-blue-vgray font-medium text-fl text-center"> Create an invoice by clicking the <br><span class="font-bold">New  <span class="hidden md:inline">Invoice</span></span> button and get started</h4>
     </div>
 </template>
 
