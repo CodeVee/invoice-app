7ae212807b337afc294539946ade2d6c5fea4332
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-12-05 12:02:47 +0100

    made views responsive

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="w-[68.8rem] mx-auto pt-4.8 lg:pt-[6.4rem] lg:pb-[5.4rem] lg:w-full lg:mx-0" v-if="state.invoice">
+    <div class="md:w-[68.8rem] mx-auto px-2.4 md:px-0 pt-3.2 md:pt-4.8 lg:pt-[6.4rem] lg:pb-[5.4rem] lg:w-full lg:mx-0" v-if="state.invoice">
         <button class="flex" @click="goHome">
             <img class="self-center" src="@/assets/images/icon-arrow-left.svg" alt="left arrow">
-            <span class="text-black dark:text-white hover:text-blue-gray dark:hover:text-blue-vgray text-fl ml-16 font-bold">Go back</span>
+            <span class="text-black dark:text-white hover:text-blue-gray dark:hover:text-blue-vgray text-fl ml-2.4 md:ml-16 font-bold">Go back</span>
         </button>
         <div class="bg-white dark:bg-blue-vdark h-[8.8rem] px-3.2 flex justify-between items-center shadow-lgs rounded-xls mt-3.2 mb-2.4">
-            <div class="flex gap-1.6">
+            <div class="flex flex-1 md:flex-none justify-between gap-1.6">
                 <span class="self-center text-fl font-medium text-blue-mgray">Status</span>
                 <InvoiceStatus :status="state.invoice.status" />
             </div>
-            <div class="flex gap-0.8">
+            <div class="hidden md:flex gap-0.8">
                 <AppButton text="Edit" type="tetiary" @btn-click="editInvoice"/>
                 <AppButton text="Delete" type="secondary" @btn-click="deleteInvoice"/>
                 <AppButton text="Mark as Paid" type="primary"/>
             </div> 
         </div>
         <div class="bg-white dark:bg-blue-vdark shadow-lgs rounded-xls px-3.2 pt-4.8 pb-3.2 lg:p-4.8">
-            <div class="flex justify-between mb-[2.1rem]">
+            <div class="flex flex-col gap-12 md:gap-0 md:flex-row justify-between mb-3.2 md:mb-8">
                 <div class="flex flex-col">
                     <h4 class="font-bold text-sl">
                         <span class="text-blue-vgray">#</span>
@@ -24,14 +24,14 @@
                     </h4>
                     <span class="text-blue-gray dark:text-blue-light text-fl font-medium">{{state.invoice.description}}</span>
                 </div>
-                <p class="text-bs text-blue-gray dark:text-blue-light text-right">
+                <p class="text-bs text-blue-gray dark:text-blue-light md:text-right">
                     {{state.invoice.senderAddress.street}} <br> 
                     {{state.invoice.senderAddress.city}} <br> 
                     {{state.invoice.senderAddress.postCode}} <br> 
                     {{state.invoice.senderAddress.country}}
                 </p>
             </div>
-            <div class="flex mb-[4.5rem]">
+            <div class="flex flex-wrap md:flex-nowrap mb-16 md:mb-[4.5rem]">
                 <div class="flex flex-col gap-3.2">
                     <div>
                         <h5 class="text-fl font-medium text-blue-gray dark:text-blue-light mb-1.2">Invoice Date</h5>
@@ -42,7 +42,7 @@
                         <h5 class="text-black dark:text-white font-bold text-ms leading-ms">{{formatDate(state.invoice.paymentDue)}}</h5>
                     </div>       
                 </div>
-                <div class="flex flex-col gap-0.8 ml-40 mr-44">
+                <div class="flex flex-col gap-0.8 ml-16 md:ml-40 md:mr-44">
                     <div>
                         <h5 class="text-fl font-medium text-blue-gray dark:text-blue-light mb-1.2">Bill To</h5>
                         <h5 class="text-black dark:text-white font-bold text-ms leading-ms">{{state.invoice.clientName}}</h5>
@@ -54,30 +54,37 @@
                         {{state.invoice.clientAddress.country}}
                     </p>
                 </div>
-                <div class="flex">
+                <div class="flex mt-3.2 md:mt-0">
                     <div>
                         <h5 class="text-fl font-medium text-blue-gray dark:text-blue-light mb-1.2">Sent To</h5>
                         <h5 class="text-black dark:text-white font-bold text-ms leading-ms">{{state.invoice.clientEmail}}</h5>
                     </div> 
                 </div>
             </div>
-            <div class="bg-blue-vlight dark:bg-blue-dark px-3.2 pt-3.2 pb-16 rounded-t-xls">
-                <div class="grid grid-cols-[28.4rem_3rem_1fr_1fr]">
+            <div class="bg-blue-vlight dark:bg-blue-dark p-2.4 md:p-0 md:px-3.2 md:pt-3.2 md:pb-16 rounded-t-xls flex flex-col gap-2.4 md:gap-3.2">
+                <div class="hidden md:grid grid-cols-[28.4rem_3rem_1fr_1fr]">
                     <span class="text-blue-gray dark:text-blue-light text-bs font-medium">Item name</span>
                     <span class="text-blue-gray dark:text-blue-light text-bs font-medium text-center">QTY.</span>
                     <span class="text-blue-gray dark:text-blue-light text-bs font-medium text-right">Price</span>
                     <span class="text-blue-gray dark:text-blue-light text-bs font-medium text-right">Total</span>
                 </div>
-                <div v-for="item in state.invoice.items" :key="item.name" class="grid grid-cols-[28.4rem_3rem_1fr_1fr] mt-3.2">
+                <div v-for="item in state.invoice.items" :key="item.name" class="hidden md:grid grid-cols-[28.4rem_3rem_1fr_1fr] mt-3.">
                     <span class="text-black dark:text-white text-fl font-bold">{{item.name}}</span>
                     <span class="text-blue-gray dark:text-blue-light text-fl font-bold text-center">{{item.quantity}}</span>
                     <span class="text-blue-gray dark:text-blue-light text-fl font-bold text-right">£ {{formatAmount(item.price)}}</span>
                     <span class="text-black dark:text-white text-fl font-bold text-right">£ {{formatAmount(item.total)}}</span>
                 </div>
+                <div v-for="item in state.invoice.items" :key="item.name" class="flex md:hidden justify-between items-center">
+                    <div class="flex flex-col gap-0.8">
+                        <span class="text-black dark:text-white text-fl font-bold">{{item.name}}</span>
+                        <span class="text-blue-gray dark:text-blue-light text-fl font-bold">{{item.quantity}} x £ {{formatAmount(item.price)}}</span>
+                    </div>
+                    <span class="text-black dark:text-white text-fl font-bold">£ {{formatAmount(item.total)}}</span>
+                </div>
             </div>
-            <div class="flex justify-between items-center bg-blue-deep dark:bg-black text-white px-3.2 h-32 rounded-b-xls">
+            <div class="flex justify-between items-center bg-blue-deep dark:bg-black text-white px-2.4 md:px-3.2 h-32 rounded-b-xls">
                 <span class="font-medium text-bs">Amount Due</span>
-                <span class="font-bold text-[2.4rem] leading-[3.2rem]">£ {{formatAmount(state.invoice.total)}}</span>
+                <span class="font-bold text-lg md:text-ls leading-ls">£ {{formatAmount(state.invoice.total)}}</span>
             </div>
         </div>
     </div>
