27f0f59cd0905964d17996bb0fe7bdf736a055d1
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-12-05 14:24:38 +0100

    resized modal

--- src/components/DeleteModal.vue
+++ src/components/DeleteModal.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex justify-center items-center w-full h-full absolute top-0 z-20" @click="$emit('close')">
-        <div class="w-[48rem] rounded-xls shadow-lgs p-4.8 bg-white dark:bg-blue-vdark relative" @click.stop="">
+    <div class="flex justify-center items-center w-full h-full px-2.4 absolute top-0 z-20" @click="$emit('close')">
+        <div class="md:w-[48rem] rounded-xls shadow-lgs p-2.4 md:p-4.8 bg-white dark:bg-blue-vdark relative" @click.stop="">
             <h4 class="text-black dark:text-white font-bold text-ts">Confirm Deletion</h4>
             <p class=" text-sm leading-md tracking-lg font-medium text-blue-vgray dark:text-blue-light mt-1.2 mb-1.6">Are you sure you want to delete invoice {{invoiceId}}? This action cannot be undone.</p>
             <div class="flex justify-end gap-0.8">
